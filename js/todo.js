04e5b8719e289912d9ee82670768a9f5a92de4ba
teja m <tejamedida897@gmail.com>
2018-01-19 09:42:15 +0530

    Added behaviour to todo class

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,23 @@
-class todo {
-  constructor() {
-
+class Todo {
+  constructor(title,desc,items) {
+    this.title = title;
+    this.desc = desc;
+    this.totalItems = 0;
+    this.items = this.setItems(items);
+  }
+  addItem(item){
+    let currentItem = this.totalItems++;
+    this.items[`item${this.totalItems}`] = item;
+  }
+  setItems(items){
+    let allItems = Object.keys(items);
+    let todo = this;
+    let todoItems = {}
+    this.items = allItems.reduce((todoItems,item)=>{
+      let currentItem = items[item];
+      todo.totalItems++;
+      todoItems[`item${this.totalItems}`] = currentItem;
+      return todoItems;
+    },todoItems)
   }
 }
