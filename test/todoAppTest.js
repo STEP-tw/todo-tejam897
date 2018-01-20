3d1a9656108733bdfee499552de33863ffebb659
Neeraj Jaiswal <neerajj85@gmail.com>
2018-01-20 12:07:00 +0530

    Added models for todoApp and TodoList
      Refactored code of app.js
      Deleted some extra view templates

---
+++ test/todoAppTest.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+let assert = chai.assert;
+let TodoApp = require('../lib/models/todoApp');
+let User = require('../lib/models/user');
+let todoApp;
+
+describe('todoApp', () => {
+  beforeEach(() => {
+    todoApp = new TodoApp({});
+  });
+  describe('addUser', () => {
+    it('should add a new user with given detail s', () => {
+      let expected = { tejam: { _name: 'teja', _userId: 'tejam', _todoLists: {}, _listId: 0 } };
+      todoApp.addUser('teja', 'tejam');
+      assert.deepEqual(todoApp._users, expected);
+    });
+  });
+  describe('getUser', () => {
+    it('should give the user with given userName', () => {
+      todoApp.addUser('teja', 'tejam');
+      let expected = { _name: 'teja', _userId: 'tejam', _todoLists: {}, _listId: 0 };
+      assert.deepEqual(todoApp.getUser('tejam'), expected)
+    });
+  });
+});
