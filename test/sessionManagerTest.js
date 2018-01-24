96430f1e913fb910742b9f1532511450eaef359a
Neeraj Jaiswal <neerajj85@gmail.com>
2018-01-24 10:26:07 +0530

    Refactored code in app and moved all handlers to handlers.js

--- test/sessionManagerTest.js
+++ test/sessionManagerTest.js
@@ -1,72 +1,72 @@
 const chai = require('chai');
 const assert = chai.assert;
-const SessionHandler = require('../lib/models/sessionHandler');
+const SessionManager = require('../lib/models/sessionManager');
 
-let sessionHandler;
+let sessionManager;
 
-describe('SessionHandler', () => {
+describe('SessionManager', () => {
   beforeEach(() => {
     const users = {
       "teja": { "username": "tejam" },
       "nrjais": { "username": "nrjais", "sessionId": "12345" }
     };
-    sessionHandler = new SessionHandler(users);
+    sessionManager = new SessionManager(users);
   });
 
   describe('isValidUser', () => {
     it('should return true for valid user', () => {
-      assert.isTrue(sessionHandler.isValidUser('teja'));
+      assert.isTrue(sessionManager.isValidUser('teja'));
     });
     it('should return false for inValid user', () => {
-      assert.isFalse(sessionHandler.isValidUser('te'));
+      assert.isFalse(sessionManager.isValidUser('te'));
     });
     it('should return false when no user is provided', () => {
-      assert.isFalse(sessionHandler.isValidUser());
-      assert.isFalse(sessionHandler.isValidUser(''));
+      assert.isFalse(sessionManager.isValidUser());
+      assert.isFalse(sessionManager.isValidUser(''));
     });
   });
   describe('getUserByUserName', () => {
     it('should return a user when user is present', () => {
       const expected = { username: 'tejam' };
-      assert.deepEqual(sessionHandler.getUserByUserName('teja'), expected);
+      assert.deepEqual(sessionManager.getUserByUserName('teja'), expected);
     });
     it('should return undefined when user is not present', () => {
-      assert.isUndefined(sessionHandler.getUserByUserName('t'));
+      assert.isUndefined(sessionManager.getUserByUserName('t'));
     });
   });
   describe('getUserBySessionId', () => {
     it('should return a user when user is present with given valid sessionId', () => {
       const expected = { username: 'nrjais', sessionId: '12345' };
-      assert.deepEqual(sessionHandler.getUserBySessionId('12345'), expected);
+      assert.deepEqual(sessionManager.getUserBySessionId('12345'), expected);
     });
     it('should return false when sessionID is invalid', () => {
-      assert.isUndefined(sessionHandler.getUserBySessionId('t'));
-      assert.isUndefined(sessionHandler.getUserBySessionId(''));
-      assert.isUndefined(sessionHandler.getUserBySessionId());
+      assert.isUndefined(sessionManager.getUserBySessionId('t'));
+      assert.isUndefined(sessionManager.getUserBySessionId(''));
+      assert.isUndefined(sessionManager.getUserBySessionId());
     });
   });
 
 
   describe('loginUser', () => {
     it('should return a sessionId when the user is present', () => {
-      assert.isString(sessionHandler.logInUser('teja'));
+      assert.isString(sessionManager.logInUser('teja'));
     });
     it('should return false when the user is not present', () => {
-      assert.isFalse(sessionHandler.logInUser('te'));
+      assert.isFalse(sessionManager.logInUser('te'));
     });
     it('should return false when no user is given', () => {
-      assert.isFalse(sessionHandler.logInUser());
+      assert.isFalse(sessionManager.logInUser());
     });
   });
   describe('logoutUser', () => {
     it('should delete sessionId of user if loggedIn', () => {
-      assert.isTrue(sessionHandler.logoutUser('12345'));
+      assert.isTrue(sessionManager.logoutUser('12345'));
     });
     it('should return false  when sessionId is inValid', () => {
-      assert.isFalse(sessionHandler.logoutUser('123'));
+      assert.isFalse(sessionManager.logoutUser('123'));
     });
     it('should return false  when sessionId is not provided', () => {
-      assert.isFalse(sessionHandler.logoutUser());
+      assert.isFalse(sessionManager.logoutUser());
     });
   });
 });
